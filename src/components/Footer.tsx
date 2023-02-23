70bdc8250ce58f2a678e707724a91bac2f16086b
João Pedro <joaopedrodamaiamoura@outlook.com>
2023-02-23 15:12:07 -0300

    feat:Footer Refatorado

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer 
-    className='w-full h-20 py-3 flex flex-col items-center justify-center text-zinc-800 font-semibold bg-gold mt-40'
+    className='w-full h-28 py-3 flex flex-col items-center justify-center text-white font-semibold bg-zinc-800 mt-40'
     >
       <p>© 2023 Todos os direitos reservados.</p>
       <p>Criado por João Pedro da Maia Moura</p>
