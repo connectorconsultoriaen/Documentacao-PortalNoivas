22de0fd2cb798a144b011b46e6a425929701ec12
João Pedro <joaopedrodamaiamoura@outlook.com>
2023-03-03 19:30:38 -0300

    Feat: Resolvendo bug do gh-pages 6

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Documentacao-PortalNoivas/",
+  base: "Documentacao-PortalNoivas/",
   plugins: [react()],
 })
